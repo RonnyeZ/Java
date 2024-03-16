43dbd62505394d34c29255d429a9675e36210428
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-16 16:14:39 -0300

    Update App.java

--- Aula01/exercicio07/src/App.java
+++ Aula01/exercicio07/src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-            //
+            // Escreva um código que receba um valor inteiro e diga se ele é divisível por cinco.
 
         entrada.close();
         }
