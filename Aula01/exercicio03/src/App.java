5a56ec3879b17c4a1bfbcec33d6effbcbaf2ff72
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-16 16:23:10 -0300

    Update App.java

--- Aula01/exercicio03/src/App.java
+++ Aula01/exercicio03/src/App.java
@@ -6,6 +6,14 @@ public class App {
 
             // Escreva um código que receba a sua idade e retorne o ano do seu nascimento
 
+            System.out.println("Digite sua idade:");
+            int idade = entrada.nextInt();
+
+
+            int ano = idade;
+
+            System.out.println("Você nasceu no ano de " + ano);
+
         entrada.close();
-        }
+    }
 }
