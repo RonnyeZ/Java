dba90271562ea6ef80b3050a8a10bcb0b6e3566a
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-16 16:40:31 -0300

    Create Ex06.java

---
+++ Aula01/exercicio06/src/Ex06.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex06 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+            // Escreva um código que receba 3 números inteiro e os exiba em ordem crescente
+
+        entrada.close();
+        }
+}
