6311b3c0fb72f2f119f1ebbcfe39561ee3f94104
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-16 16:14:31 -0300

    Update App.java

--- Aula01/exercicio04/src/App.java
+++ Aula01/exercicio04/src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-            //
+            //Escreva um código que receba o valor do raio de uma circunferência e retorne a área desta circunferência.
 
         entrada.close();
         }
