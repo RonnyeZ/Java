0fd9b398311281f965851f196d9b9d3b61d44b32
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-16 16:14:35 -0300

    Update App.java

--- Aula01/exercicio05/src/App.java
+++ Aula01/exercicio05/src/App.java
@@ -4,7 +4,14 @@ public class App {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-            //
+            /*
+            Uma empresa possui três cargos e seus respectivos salários, sendo eles:
+                Gerente - R$ 8.500,00
+                Analista - R$ 5.000,00
+                Suporte - R$ 3.000,00
+            Será dado um reajuste de 12% nos salários do Gerente e Analista, e 15% no salário do Suporte.
+            
+            Escreva um código que faça o reajuste e exiba os novos valores. */
 
         entrada.close();
         }
