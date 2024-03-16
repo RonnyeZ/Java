72a18e3c8f4d8895bc131bed60c16d10d731861c
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-16 16:40:49 -0300

    Create Ex04.java

---
+++ Aula01/exercicio04/src/Ex04.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+            //Escreva um código que receba o valor do raio de uma circunferência e retorne a área desta circunferência.
+
+        entrada.close();
+        }
+}
