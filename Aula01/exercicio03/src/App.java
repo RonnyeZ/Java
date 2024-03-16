cbe6e86b2c961cf232f2bc55dc745f4295df31cd
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-16 16:14:27 -0300

    Update App.java

--- Aula01/exercicio03/src/App.java
+++ Aula01/exercicio03/src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-            // 
+            // Escreva um código que receba a sua idade e retorne o ano do seu nascimento
 
         entrada.close();
         }
