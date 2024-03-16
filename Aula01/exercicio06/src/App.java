8b74d3ce5f097d49a7c897a6f478227b36b6047a
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-16 16:14:37 -0300

    Update App.java

--- Aula01/exercicio06/src/App.java
+++ Aula01/exercicio06/src/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-            //
+            // Escreva um código que receba 3 números inteiro e os exiba em ordem crescente
 
         entrada.close();
         }
