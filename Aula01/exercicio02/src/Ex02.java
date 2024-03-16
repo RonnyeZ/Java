af0a71b13ccc03be055c3ca20751a51995e01bd5
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-16 16:40:40 -0300

    Create Ex02.java

---
+++ Aula01/exercicio02/src/Ex02.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+            // Escreva um código que receba 3 valores inteiro (a, b e c). Utilize esses valores para encontrar o valor de delta. Caso o valor de delta seja menor que zero, informar que a equação não possui raízes reais. Se for maior ou igual a zero, encontre e informe os valores de x1 e x2. Para raiz quadrada precisaremos importar a biblioteca “math” e usar o comando math.sqrt().
+
+        entrada.close();
+        }
+}
