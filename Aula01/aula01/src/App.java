04bc4ccc231fb2a649f68075de4fa0e498304179
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-16 14:26:32 -0300

    Initial commit

---
+++ Aula01/aula01/src/App.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("--- Informando os Lados do Triângulo 1 ---");
+        Double total = entrada.nextDouble();
+        
+    }
+}
