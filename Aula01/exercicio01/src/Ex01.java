9486b7ba715bfd125049cc1ee1cfd045744b7550
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-16 16:40:36 -0300

    Create Ex01.java

---
+++ Aula01/exercicio01/src/Ex01.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        // Escreva um código que mostre um menu para o usuário com a operações matemáticas básicas (soma, subtração, multiplicação e divisão). Peça para o usuári escolher uma operação e informar 2 valores. Faça a operação de acordo com a escolha do usuário e exiba o resultado.
+
+        System.out.println("Escolha uma operação entre +, -, * /");
+        String simbolo = entrada.nextLine();
+
+        System.out.println("Escolha o 1° valor.");
+        double valor1 = entrada.nextDouble();
+
+        System.out.println("Escolha o 2° valor.");
+        double valor2 = entrada.nextDouble();
+
+        if(simbolo.equals("+")) {
+            double resposta = valor1 + valor2;
+            System.out.println("A resposta é: " + resposta);
+
+        } else if(simbolo.equals("-")) {
+            double resposta = valor1 - valor2;
+            System.out.println("A resposta é: " + resposta);
+
+        } else if(simbolo.equals("*")) {
+            double resposta = valor1 * valor2;
+            System.out.println("A resposta é: " + resposta);
+
+        } else if(simbolo.equals("/")) {
+            double resposta = valor1 / valor2;
+            System.out.println("A resposta é: " + resposta);
+        }
+
+        entrada.close();
+    }
+}
