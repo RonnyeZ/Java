b14bdecb1238acd377d92c176295102c6e1cb11d
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2024-03-18 11:43:44 -0300

    Add files via upload

--- TrianguloSemPOO/src/TrianguloPrincipal.java
+++ TrianguloSemPOO/src/TrianguloPrincipal.java
@@ -10,25 +10,36 @@ public class TrianguloPrincipal {
 		
 		Triangulo t2 = new Triangulo();
 		
-		System.out.println("----------");
+		// Lados do 1° Triangulo
+		System.out.println("Defina o primerio lado do Triangulo 1");
 		t1.a1 = entrada.nextDouble();
 		
-		System.out.println("----------");
+		System.out.println("Defina o segundo lado do Triangulo 1");
 		t1.a2 = entrada.nextDouble();
 		
-		System.out.println("----------");
+		System.out.println("Defina o terceiro lado do Triangulo 1");
 		t1.a3 = entrada.nextDouble();
 		
-		
-		System.out.println("----------");
+		//Lados do 2° Triangulo
+		System.out.println("Defina o primerio lado do Triangulo 2");
 		t2.a1 = entrada.nextDouble();
 		
-		System.out.println("----------");
+		System.out.println("Defina o segundo lado do Triangulo 2");
 		t2.a2 = entrada.nextDouble();
 		
-		System.out.println("----------");
+		System.out.println("Defina o terceiro lado do Triangulo 2");
 		t2.a3 = entrada.nextDouble();
 		
+		
+		double p1 = (t1.a1 + t1.a2 + t1.a3) / 2; 
+		double p2 = (t2.a1 + t2.a2 + t2.a3) / 2;
+		
+		double a1 = Math.sqrt(p1 * (p1 - t1.a1) * (p1 - t1.a2) * (p1 - t1.a3));
+		double a2 = Math.sqrt(p2 * (p2 - t2.a1) * (p2 - t2.a2) * (p2 - t2.a3));
+		
+		System.out.println("A area do triangulo 1 é:" + a1);
+		System.out.println("");
+		System.out.println("A area do triangulo 2 é:" + a2);
 		
 		entrada.close();
 	}
