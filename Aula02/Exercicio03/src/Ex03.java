0d7903b2a8e2eab8ee52c1741463736b7fc76d5c
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 20:57:46 -0300

    Update Ex03.java

--- Aula02/Exercicio03/src/Ex03.java
+++ Aula02/Exercicio03/src/Ex03.java
@@ -13,23 +13,28 @@ public class Ex03 {
 
         int opcao = entrada.nextInt();
 
+        // Se a opção for diferente de 1 ou 2, causa Error
         if (opcao != 1 && opcao != 2){
 
             System.out.println("Sinto muito, porém o valor inserido é inválido.");
 
+            // Se valor for 1 ou 2, da proseguimento
         } else {
+
+            // Se a variavel "opcao" for igual a 1, inicia o while, a qual vai repetir o mesmo processo.
             while (opcao == 1) { 
 
-                System.out.println("");
+                System.out.println(""); // Espaço
 
                 System.out.println("Qual serviço você deseja agora?");
                 System.out.println("1 - Continuar");
                 System.out.println("2 - Sair"); 
-                opcao = entrada.nextInt(); 
+                opcao = entrada.nextInt(); // Receber novo valor de "opcao"
 
-                System.out.println("");   
+                System.out.println(""); // Espaço
             }
 
+            // Se a variavel "opcao" for 2, ela passa por todas as condições e chega aqui.
             System.out.println("Obrigado por usar nosso sistema!");
         }
 
