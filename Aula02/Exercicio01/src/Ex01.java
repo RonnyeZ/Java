1918ff80e088a98346ce94dce994f9a9f9e8af31
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 17:40:24 -0300

    Create Ex01.java

---
+++ Aula02/Exercicio01/src/Ex01.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+
+        /*
+         Escreva um código que receba 2 valores do tipo inteiro, faça sua soma e informe se o resultado é par ou ímpar.
+        */
+
+        System.out.println("Defina dois valores inteiros:");
+        int x1 = entrada.nextInt();
+        int x2 = entrada.nextInt();
+
+        int valor = x1 + x2;
+
+        if (valor % 2 == 0) {
+            System.out.println("A soma dos valores resulta num número par, sendo esse resultado" + valor);
+        } else {
+            System.out.println("A soma dos valores resulta num número ímpar, sendo esse resultado " + valor);
+        }
+
+        entrada.close();
+    }
+}
