ca20107eae458495a22784569e43f7f3bd2b3ab5
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 20:12:58 -0300

    Update Ex04.java

--- Aula02/Exercicio04/src/Ex04.java
+++ Aula02/Exercicio04/src/Ex04.java
@@ -1,6 +1,5 @@
 public class Ex04 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         /* 
          Escreva um código que receba um valor de login e um valor de senha. Caso os valores estejam corretos, retornar ao usuário: “Você está logado”. Caso contrário, informar: “Login ou senha incorretos”.
          Defina um valor padrão para login e senha.
