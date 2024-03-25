a69e7228649994438f01e1145367b7b29090d23a
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 17:41:51 -0300

    Create Ex06.java

---
+++ Aula02/Exercicio06/src/Ex06.java
@@ -0,0 +1,8 @@
+public class Ex06 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        /* 
+         Escreva um código que receba um valor inteiro de 0 a 10. Exiba a tabuada de 0 a 10 do valor informado.
+        */
+    }
+}
