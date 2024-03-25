196847d0641bfe574ed25ac01c0d98e73e61f769
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 20:13:13 -0300

    Update Ex08.java

--- Aula02/Exercicio08/src/Ex08.java
+++ Aula02/Exercicio08/src/Ex08.java
@@ -1,6 +1,5 @@
 public class Ex08 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         /* 
          Escreva um programa que receba a idade de n pessoas e ao final informe a quantidade de pessoas com idade entre 0 e 25 anos, 26 e 60 anos e maior que 60 anos. Continue recebendo idades até que o usuário informe que não quer mais receber idades.
         */
