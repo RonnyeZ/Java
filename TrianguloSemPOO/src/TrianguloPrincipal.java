895fa385bbe5bad673311b4a811820b5c31e7e34
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 11:32:50 -0300

    Aula de POO 25/03
    Aula de Orientação de Classe e objeto com prof. Junio

--- TrianguloSemPOO/src/TrianguloPrincipal.java
+++ TrianguloSemPOO/src/TrianguloPrincipal.java
@@ -11,35 +11,48 @@ public class TrianguloPrincipal {
 		Triangulo t2 = new Triangulo();
 		
 		// Lados do 1° Triangulo
-		System.out.println("Defina o primerio lado do Triangulo 1");
+		System.out.println("Defina os lados do 1° Triangulo");
 		t1.a1 = entrada.nextDouble();
-		
-		System.out.println("Defina o segundo lado do Triangulo 1");
 		t1.a2 = entrada.nextDouble();
-		
-		System.out.println("Defina o terceiro lado do Triangulo 1");
 		t1.a3 = entrada.nextDouble();
 		
 		//Lados do 2° Triangulo
-		System.out.println("Defina o primerio lado do Triangulo 2");
+		System.out.println("Defina os lados do 2° Triangulo");
 		t2.a1 = entrada.nextDouble();
-		
-		System.out.println("Defina o segundo lado do Triangulo 2");
 		t2.a2 = entrada.nextDouble();
-		
-		System.out.println("Defina o terceiro lado do Triangulo 2");
 		t2.a3 = entrada.nextDouble();
 		
+		double p1 = t1.calculoP();
+		System.out.println("Valor do Perimetro do 1° Triangulo: " + p1);
 		
+		double p2 = t1.calculoP();
+		System.out.println("Valor do Perimetro do 2° Triangulo: " + p2);
+		
+		System.out.println("");
+		
+		double area1 = t1.areaDoTriangulo();
+		System.out.println("Valor da Área do 1° Triangulo: " + area1);
+
+		double area2 = t2.areaDoTriangulo();
+		System.out.println("Valor da Área do 2° Triangulo: " + area2);
+		
+		System.out.println("");
+
+		//Resultado
+		t1.areaMaior(area1,area2);
+		
+		
+		/*
 		double p1 = (t1.a1 + t1.a2 + t1.a3) / 2; 
 		double p2 = (t2.a1 + t2.a2 + t2.a3) / 2;
 		
 		double a1 = Math.sqrt(p1 * (p1 - t1.a1) * (p1 - t1.a2) * (p1 - t1.a3));
 		double a2 = Math.sqrt(p2 * (p2 - t2.a1) * (p2 - t2.a2) * (p2 - t2.a3));
-		
+		ath.sq
 		System.out.println("A area do triangulo 1 é:" + a1);
 		System.out.println("");
 		System.out.println("A area do triangulo 2 é:" + a2);
+		*/
 		
 		entrada.close();
 	}
