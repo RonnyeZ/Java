fbcae0e4debfdd985e8950eed98336ba5d527ec0
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 17:41:40 -0300

    Create Ex05.java

---
+++ Aula02/Exercicio05/src/Ex05.java
@@ -0,0 +1,8 @@
+public class Ex05 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        /* 
+         Escreva um código que receba 6 valores do usuário, exiba a sua soma e a sua média. Fazer usando laços de repetição.
+        */
+    }
+}
