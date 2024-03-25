34e0b8e2d03dee9713d77c6656b167a52ed7a051
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 18:05:28 -0300

    Update Ex02.java

--- Aula02/Exercicio02/src/Ex02.java
+++ Aula02/Exercicio02/src/Ex02.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
+
 public class Ex02 {
     public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
         /*
          Escreva um código que receba um valor inteiro e caso o valor informado seja par, imprimir os valores pares de zero até o valor informado, caso seja ímpar, informar os valores ímpares de 1 ao valor informado.
         */
+
+        System.out.println("Defina um valor inteiro.");
+        int valor = entrada.nextInt();
+        System.out.println("");
+
+        // Se valor for igual a Par
+        if (valor % 2 == 0) {
+
+            // Imprimir valores pares de 0 até o valor definido
+            for (int i = 0; i < valor; i++) {
+
+                //Imprimir somente valores pares
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                }
+            }
+
+        // Se valor não for igual a Par, ou seja, for Impar
+        } else { 
+
+            // Imprimir valores impares de 0 até o valor definido
+            for (int i = 0; i < valor; i++) {
+
+                // Imprimir somente valor Impares
+                if (i % 2 != 0) {
+                    System.out.println(i);
+                }
+            }
+        }
+
+        entrada.close();
     }
 }
