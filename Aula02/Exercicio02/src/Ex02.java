077f89ba2ed30cf74a1e0e129d244edbd69b691d
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 17:40:57 -0300

    Create Ex02.java

---
+++ Aula02/Exercicio02/src/Ex02.java
@@ -0,0 +1,7 @@
+public class Ex02 {
+    public static void main(String[] args) throws Exception {
+        /*
+         Escreva um código que receba um valor inteiro e caso o valor informado seja par, imprimir os valores pares de zero até o valor informado, caso seja ímpar, informar os valores ímpares de 1 ao valor informado.
+        */
+    }
+}
