3a056c27d11e931a712ebb6c7e9aaf0c77604913
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 17:42:00 -0300

    Create Ex07.java

---
+++ Aula02/Exercicio07/src/Ex07.java
@@ -0,0 +1,13 @@
+public class Ex07 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        /* 
+         Escreva um código que escreva a sequência Fibonacci até o valor informado pelo usuário.
+         
+            Ex.: Caso o usuário informe o valor 100, o resultado será:
+            0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89
+
+         Pesquisem sobre a sequência Fibonacci.
+        */
+    }
+}
