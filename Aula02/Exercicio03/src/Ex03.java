68dbea902c6ae13d465a068ceb431ae410bb79d0
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 20:11:35 -0300

    Update Ex03.java

--- Aula02/Exercicio03/src/Ex03.java
+++ Aula02/Exercicio03/src/Ex03.java
@@ -1,6 +1,5 @@
 public class Ex03 {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
         /*
          Escreva um código que exiba um menu para o usuário com duas opções (1 - continuar, 2 - sair). Exiba o menu até que o usuário digite 2. Caso o usuário informe outro valor diferente de 1 e 2, retornar que o valor é inválido.
         */
