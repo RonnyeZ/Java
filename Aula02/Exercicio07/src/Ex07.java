f32606da44da958180799be93413bdf7f516e554
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 21:51:41 -0300

    Update Ex07.java

--- Aula02/Exercicio07/src/Ex07.java
+++ Aula02/Exercicio07/src/Ex07.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Ex07 {
     public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
         /* 
          Escreva um código que escreva a sequência Fibonacci até o valor informado pelo usuário.
          
@@ -8,5 +11,9 @@ public class Ex07 {
 
          Pesquisem sobre a sequência Fibonacci.
         */
+
+        System.out.println("");
+
+        entrada.close();
     }
 }
