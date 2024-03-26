db91d55fc196d9d4610deb732a212c391726dc94
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 21:51:46 -0300

    Update Ex08.java

--- Aula02/Exercicio08/src/Ex08.java
+++ Aula02/Exercicio08/src/Ex08.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+
 public class Ex08 {
     public static void main(String[] args) throws Exception {
+        Scanner entrada= new Scanner(System.in);
         /* 
          Escreva um programa que receba a idade de n pessoas e ao final informe a quantidade de pessoas com idade entre 0 e 25 anos, 26 e 60 anos e maior que 60 anos. Continue recebendo idades até que o usuário informe que não quer mais receber idades.
         */
+
+        System.out.println("");
+
+        entrada.close();
     }
 }
