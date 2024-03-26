89efeddaf5a08cf71664ecb7e169843ee0d41f7f
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 21:35:31 -0300

    Update Ex06.java

--- Aula02/Exercicio06/src/Ex06.java
+++ Aula02/Exercicio06/src/Ex06.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Ex06 {
     public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+
         /* 
          Escreva um código que receba um valor inteiro de 0 a 10. Exiba a tabuada de 0 a 10 do valor informado.
         */
+
+        int valor = 0;
+
+        for (int i = 0; i < valor; i++) {
+            System.out.println(""); // Espaço
+
+            System.out.println("Digite um numero");
+        }
+
+        entrada.close();
     }
 }
