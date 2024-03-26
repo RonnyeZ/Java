7165fd0a8a3c79de12437b8b5dd96dda36e14ba0
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-03-25 21:31:31 -0300

    Update Ex05.java

--- Aula02/Exercicio05/src/Ex05.java
+++ Aula02/Exercicio05/src/Ex05.java
@@ -1,7 +1,41 @@
+import java.util.Scanner;
+
 public class Ex05 {
     public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+
         /* 
          Escreva um código que receba 6 valores do usuário, exiba a sua soma e a sua média. Fazer usando laços de repetição.
         */
+
+        System.out.println("Informe 6 valores para recebe a soma e a media dentre eles.");
+
+        // Declaração das variáveis
+        
+        double numero = 0;
+        double soma = 0; 
+        int contador = 0;
+
+        // Laço de repetição para receber os 6 valores do usuário
+        for (int i = 0; i < 6; i++) {
+
+            System.out.println(""); // Espaço
+
+            System.out.println("Digite um número: ");
+            numero = entrada.nextDouble();
+
+            // Soma dos valores
+            soma = soma + numero;
+
+            // Incremento do contador
+            contador++;
+        }
+
+        double media = soma / contador;
+
+        System.out.println("A soma dos valores é: " + soma);
+        System.out.println("A media dos valores é: " + media);
+
+        entrada.close();
     }
 }
