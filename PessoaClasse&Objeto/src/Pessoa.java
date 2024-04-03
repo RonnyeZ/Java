b8f147fda467bea8634f386ef862cd5aaf2e0cac
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-04-03 17:54:15 -0300

    Create Pessoa.java

---
+++ PessoaClasse&Objeto/src/Pessoa.java
@@ -0,0 +1,17 @@
+public class Pessoa {
+
+    String nome;
+    String sobreNome;
+    int anoNascimento;
+    
+    public String juntaNome() {
+        String nomeCompleto = this.nome + " " + this.sobreNome;
+        return nomeCompleto;
+    }
+
+    public void informaIdade() {
+        int idade = 2024 - this.anoNascimento;
+        System.out.println("Sua Idade é " + idade);
+    }
+}
+    
