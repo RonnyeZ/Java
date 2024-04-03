5006285fb6944fb8dbcce5865689c8a0d7d9e4cc
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-04-03 17:43:49 -0300

    Delete App.java

--- Aula04/BancoDeDados/src/App.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner entrada = new Scanner(System.in);
-
-        entrada.close();
-    }
-}
