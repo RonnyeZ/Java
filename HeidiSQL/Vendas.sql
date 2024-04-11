7474302afcb813ea55e8ef39a6ed58585d3c4107
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2024-04-11 10:40:44 -0300

    Update Vendas.sql

--- HeidiSQL/Vendas.sql
+++ HeidiSQL/Vendas.sql
@@ -6,15 +6,15 @@ VALUES
 SELECT nome, descricao, carrinho.quantidade, valor,
 (valor * carrinho.quantidade) AS subtotal
 FROM carrinho 
-INNER JOIN cliente ON cliente = cpf
-INNER JOIN produto ON produto = codigo_produto
+INNER JOIN cliente ON cliente = cpf /*Faz a conexão da tabela Carrinho com tabela Cliente*/
+INNER JOIN produto ON produto = codigo_produto /*Faz a conexão com a tabela Produto*/
 WHERE cpf = "12345654445"
 
 
-DELETE FROM carrinho
+DELETE FROM carrinho /*delete do carrinho*/
 
 
-INSERT INTO carrinho (cod_carrinho, cliente, produto, quantidade)
+INSERT INTO carrinho (cod_carrinho, cliente, produto, quantidade) /*Adiciona itens no carrinho*/
 VALUES
 (2, "12345654445", 240002, 2),
 (2, "12345654445", 240006, 1),
@@ -30,21 +30,29 @@ VALUES
 (4, "98765432100", 240006, 1),
 (5, "12345678900", 240001, 1);
 
-SELECT descricao, COUNT(carrinho.quantidade) AS vendidos, 
+SELECT descricao, COUNT(carrinho.quantidade) AS vendidos, /*Mostra Descricao, quantidades, valor e valor total dos itens do carrinho*/ 
 valor, (valor * COUNT(carrinho.quantidade)) AS subtotal FROM carrinho
 INNER JOIN produto ON produto = codigo_produto
 GROUP BY descricao, valor
 
-SELECT SUM(valor) AS Total FROM carrinho
+SELECT SUM(valor) AS Total FROM carrinho /*Faz a soma dos valores dos produtos no carrinho*/
 INNER JOIN produto ON produto = codigo_produto
 
-INSERT INTO produto (codigo_produto, descricao, quantidade, valor)
+INSERT INTO produto (codigo_produto, descricao, quantidade, valor) /*Adiciona novos itens a tabela produto*/
 VALUES
 (240008, "Pulseira", 10, 25.00),
 (240009, "Colar", 15, 175.00);
 
-SELECT descricao FROM produto
+SELECT descricao FROM produto /*Mostrar todos os itens no carrinho*/
 WHERE codigo_produto IN (SELECT produto FROM carrinho)
 
-SELECT descricao FROM produto
+SELECT descricao, valor FROM produto /*Mostrar todos os itens que não estão no carrinho*/
+WHERE codigo_produto NOT IN (SELECT produto FROM carrinho)
+
+UPDATE produto /*Produtos que não estejam no carrinho reduzem 15% do valor*/
+SET valor = valor - (valor * 0.15)
+WHERE codigo_produto NOT IN (SELECT produto FROM carrinho)
+
+UPDATE produto /*Voltar ao valor inicial*/
+SET valor = valor + (valor * 0.1764705882352941176470588235294)
 WHERE codigo_produto NOT IN (SELECT produto FROM carrinho)
