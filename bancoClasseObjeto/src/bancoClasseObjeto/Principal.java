bf522eae8df599a60641cb840cbb30375c28f064
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2024-04-22 10:55:26 -0300

    Update Principal.java

--- bancoClasseObjeto/src/bancoClasseObjeto/Principal.java
+++ bancoClasseObjeto/src/bancoClasseObjeto/Principal.java
@@ -1,55 +1,63 @@
 package bancoClasseObjeto;
 
-import java.util.Scanner;
+public class Account {
+	private int numero;
+	
+	private String nome;
+	
+	private double saldo;
+	
 
-public class Principal {
-
-	public static void main(String[] args) {
-		Scanner entrada = new Scanner(System.in);
-		
-		Account conta;
-		
-		System.out.println("Informe o numero da conta: ");
-		int numConta = entrada.nextInt();
-		
-		System.out.println("Informe o seu nome: ");
-		entrada.nextLine();
-		String nomeTitular = entrada.nextLine();
-		
-		System.out.print("Você deseja efetuar um deposito inicial? (y/n): ");
-		char respostaDeposito = entrada.next().toLowerCase().charAt(0); 
-		//"toLowerCase()" serve para deminuir a letra para minuscula 
-		//"charAt(x)" serve para pegar a letra da posição X
-		
-		if (respostaDeposito == 'y') {
-			
-			System.out.print("Informe o valor de deposito:");
-			double valorDeposito = entrada.nextDouble();
-			conta = new Account(numConta, nomeTitular, valorDeposito);
-			
-		} else {
-			
-			conta = new Account(numConta, nomeTitular);
-		}
-		
-		System.out.println(""); //Espaço
-		
-		System.out.println("Dados da Conta: ");
-		System.out.println(conta);
-		
-		System.out.println(""); //Espaço
-		
-		System.out.print("Informe o valor de deposito: ");
-		double valorDoDeposito = entrada.nextDouble();
-		conta.deposito(valorDoDeposito);
-		
-		System.out.println(""); //Espaço
-		
-		System.out.println("Atualização dos dados da conta: ");
-		System.out.println(conta);
-		
-		
-		entrada.close();
+	public Account(int numero, String nome) {
+		this.numero = numero;
+		this.nome = nome;
 	}
 
+	public Account(int numero, String nome, double valorDeposito) {
+		this.numero = numero;
+		this.nome = nome;
+		deposito(valorDeposito);
+	}
+	
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	
+	public void deposito(double amount) {
+		this.saldo += amount;
+	}
+	
+	public void saque(double amount) {
+		this.saldo -= amount;
+	}
+
+	@Override
+	public String toString() {
+		return  "Conta: " + this.numero + 
+				" - Titular: " + this.nome + 
+				" - Saldo Atual: R$ " + this.saldo;
+	}
+	
+	
 }
