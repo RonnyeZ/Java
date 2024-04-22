4f1e318193fcb7ce82fb62395168ff2ea839d3f1
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2024-04-22 10:38:53 -0300

    Update Principal.java

--- bancoClasseObjeto/src/bancoClasseObjeto/Principal.java
+++ bancoClasseObjeto/src/bancoClasseObjeto/Principal.java
@@ -16,22 +16,37 @@ public class Principal {
 		entrada.nextLine();
 		String nomeTitular = entrada.nextLine();
 		
-		System.out.println("Você deseja efetuar um deposito inicial? (y/n)");
+		System.out.print("Você deseja efetuar um deposito inicial? (y/n): ");
 		char respostaDeposito = entrada.next().toLowerCase().charAt(0); 
-		// "toLowerCase()" serve para deminuir a letra para minuscola 
-		//"charAt()" serve para pegar a letra da posição definida
+		//"toLowerCase()" serve para deminuir a letra para minuscula 
+		//"charAt(x)" serve para pegar a letra da posição X
 		
 		if (respostaDeposito == 'y') {
 			
-			System.out.println("Informe o valor de deposito:");
+			System.out.print("Informe o valor de deposito:");
 			double valorDeposito = entrada.nextDouble();
 			conta = new Account(numConta, nomeTitular, valorDeposito);
 			
 		} else {
 			
 			conta = new Account(numConta, nomeTitular);
-			
 		}
+		
+		System.out.println(""); //Espaço
+		
+		System.out.println("Dados da Conta: ");
+		System.out.println(conta);
+		
+		System.out.println(""); //Espaço
+		
+		System.out.print("Informe o valor de deposito: ");
+		double valorDoDeposito = entrada.nextDouble();
+		conta.deposito(valorDoDeposito);
+		
+		System.out.println(""); //Espaço
+		
+		System.out.println("Atualização dos dados da conta: ");
+		System.out.println(conta);
 		
 		
 		entrada.close();
