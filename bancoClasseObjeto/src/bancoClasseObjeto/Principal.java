0bc62ddf603e6f2f1d80fbf57eba874a1e4e6f86
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2024-04-22 11:32:26 -0300

    Update Principal.java

--- bancoClasseObjeto/src/bancoClasseObjeto/Principal.java
+++ bancoClasseObjeto/src/bancoClasseObjeto/Principal.java
@@ -1,63 +1,112 @@
 package bancoClasseObjeto;
 
-public class Account {
-	private int numero;
-	
-	private String nome;
-	
-	private double saldo;
-	
+import java.util.Scanner;
 
-	public Account(int numero, String nome) {
-		this.numero = numero;
-		this.nome = nome;
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		Account conta;
+		
+		System.out.println("Informe o numero da conta: ");
+		int numConta = entrada.nextInt();
+		
+		System.out.println("Informe o seu nome: ");
+		entrada.nextLine();
+		String nomeTitular = entrada.nextLine();
+		
+		System.out.print("Você deseja efetuar um deposito inicial? (y/n): ");
+		char respostaDeposito = entrada.next().toLowerCase().charAt(0); 
+		//"toLowerCase()" serve para deminuir a letra para minuscula 
+		//"charAt(x)" serve para pegar a letra da posição X
+		
+		if (respostaDeposito == 'y') {
+			
+			System.out.print("Informe o valor de deposito:");
+			double valorDeposito = entrada.nextDouble();
+			conta = new Account(numConta, nomeTitular, valorDeposito);
+			
+		} else {
+			
+			conta = new Account(numConta, nomeTitular);
+		}
+		
+		System.out.println(""); //Espaço
+		
+		System.out.println("Dados da Conta: ");
+		System.out.println(conta);
+		
+		
+		System.out.println(""); //Espaço
+		
+		System.out.print("Informe o valor de deposito: ");
+		double valorDoDeposito = entrada.nextDouble();
+		conta.deposito(valorDoDeposito);
+		
+		System.out.println(""); //Espaço
+		
+		System.out.println("Atualização dos dados da conta: ");
+		System.out.println(conta);
+		
+		System.out.println(""); //Espaço
+		
+		System.out.print("Informe o valor de saque: ");
+		double valorDeSaque = entrada.nextDouble();
+		conta.saque(valorDeSaque);
+		
+		System.out.println(""); //Espaço
+		
+		System.out.println("Atualização dos dados da conta: ");
+		System.out.println(conta);
+		
+		
+		/*
+		System.out.println("Você deseja atualizar seu saldo? (y/n)");
+		char condicao = entrada.next().toLowerCase().charAt(0);
+		
+		while (condicao == 'y') {
+			
+			System.out.println(""); //Espaço
+			
+			System.out.println("Escolha seu procedimento:");
+			System.out.println("1 - Deposito");
+			System.out.println("2 - Saque");
+			
+			int escolha = entrada.nextInt();
+			
+			if (escolha == 1) {
+				
+				System.out.println(""); //Espaço
+				
+				System.out.print("Informe o valor de deposito: ");
+				double vdd = entrada.nextDouble();
+				conta.deposito(vdd);
+				
+			} else if (escolha == 2) {
+				
+				System.out.println(""); //Espaço
+				
+				System.out.print("Informe o valor de saque: ");
+				double vds = entrada.nextDouble();
+				conta.saque(vds);
+				
+			}
+			
+			System.out.println(""); //Espaço
+			
+			System.out.println("Atualização dos dados da conta: ");
+			System.out.println(conta);
+			
+			System.out.println(""); //Espaço
+			
+			System.out.println("Você deseja prosseguir?");
+			escolha = condicao = entrada.next().toLowerCase().charAt(0);
+			
+		}*/
+		
+		
+		entrada.close();
 	}
 
-	public Account(int numero, String nome, double valorDeposito) {
-		this.numero = numero;
-		this.nome = nome;
-		deposito(valorDeposito);
-	}
-	
-
-	public int getNumero() {
-		return numero;
-	}
-
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public double getSaldo() {
-		return saldo;
-	}
-
-	public void setSaldo(double saldo) {
-		this.saldo = saldo;
-	}
-	
-	public void deposito(double amount) {
-		this.saldo += amount;
-	}
-	
-	public void saque(double amount) {
-		this.saldo -= amount;
-	}
-
-	@Override
-	public String toString() {
-		return  "Conta: " + this.numero + 
-				" - Titular: " + this.nome + 
-				" - Saldo Atual: R$ " + this.saldo;
-	}
-	
-	
 }
