561a64d43623521d7dd3602b397d9d4ba87e16ac
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2024-04-26 10:08:23 -0300

    Add files via upload

---
+++ mercadoClasseObjeto/src/mercadoClasseObjeto/Client.java
@@ -0,0 +1,26 @@
+package mercadoClasseObjeto;
+
+public class Client {
+	
+	private String name;
+	
+	private double price;
+	
+	//Metodos
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
---
+++ mercadoClasseObjeto/src/mercadoClasseObjeto/Product.java
@@ -0,0 +1,37 @@
+package mercadoClasseObjeto;
+
+public class Product {
+	
+	private String name;
+	
+	private String email;
+	
+	private int birthDate;
+	
+	
+	//Metodos
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(int birthDate) {
+		this.birthDate = birthDate;
+	}
+
+}
