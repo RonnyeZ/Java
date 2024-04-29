1416fbe0e87836efc136055b6adb688c53769078
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2024-04-29 12:00:19 -0300

    Add files via upload

---
+++ herancaClasseObjeto/src/heranca/Secretarias.java
@@ -0,0 +1,16 @@
+package heranca;
+
+public class Secretarias extends Funcionario {
+	
+	private int ramalNumber;
+	
+
+	public int getRamalNumber() {
+		return ramalNumber;
+	}
+
+	public void setRamalNumber(int ramalNumber) {
+		this.ramalNumber = ramalNumber;
+	}
+
+}
---
+++ herancaClasseObjeto/src/heranca/Telefonistas.java
@@ -0,0 +1,17 @@
+package heranca;
+
+public class Telefonistas extends Funcionario {
+	
+	private int code;
+	
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+	
+
+}
