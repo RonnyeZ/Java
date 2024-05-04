f2977e4b6c11bd4366bc5215164c2e1909dd6353
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-05-04 17:35:07 -0300

    Create Ex01.java

---
+++ NotaDeProva/Exercicios/src/Ex01.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Defina um valor inicial");
+        int x = entrada.nextInt();
+        System.out.println("Defina um valor final");
+        int y = entrada.nextInt();
+
+        System.out.println("");
+
+        if (y - x <= 1){
+
+            System.out.println(0);
+
+        } else {
+            
+            for (int i = x+1; i < y; i++) {
+                System.out.println(i);
+            }
+        }
+
+        entrada.close();
+    }
+}
