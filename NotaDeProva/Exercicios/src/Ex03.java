f7dcb9db12edc20acb2fa7a63aae80b2160c8326
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-05-04 17:35:16 -0300

    Create Ex03.java

---
+++ NotaDeProva/Exercicios/src/Ex03.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ex03 {
+    public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+
+      
+
+        entrada.close();
+    }
+}
