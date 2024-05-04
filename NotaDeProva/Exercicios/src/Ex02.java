87eb6d46409adb4c4738c8f286efffe7ff6acd44
Rony <121881238+RonnyeZ@users.noreply.github.com>
2024-05-04 17:35:11 -0300

    Create Ex02.java

---
+++ NotaDeProva/Exercicios/src/Ex02.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) throws Exception {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Informe 10 valores");
+        
+        int contador = 0;
+        int resultado = 0;
+
+        while (contador < 10){
+
+            int valor = entrada.nextInt();
+
+            if (valor % 2 == 0){
+
+                resultado += valor;
+
+            } else {
+
+                resultado -= valor;
+
+            }
+
+            contador++;
+        }
+
+        System.out.println(resultado);
+
+        entrada.close();
+    }
+}
