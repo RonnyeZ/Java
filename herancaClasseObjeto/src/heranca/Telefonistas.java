cfb35871501480c01b35e61b77c792814d35f996
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2024-05-06 10:43:38 -0300

    Update Telefonistas.java

--- herancaClasseObjeto/src/heranca/Telefonistas.java
+++ herancaClasseObjeto/src/heranca/Telefonistas.java
@@ -4,6 +4,20 @@ public class Telefonistas extends Funcionario {
 	
 	private int code;
 	
+	public Telefonistas() {
+		
+	}
+	
+	public Telefonistas(int code) {
+		super();
+		this.code = code;
+		
+	}
+	
+	public void mostrarDados() {
+		super.mostrarDados();
+		System.out.println("Estação de Trabalho: " + this.code);
+	}
 
 	public int getCode() {
 		return code;
