d14a1c65ad45ec2cb5e48cb5a156cdf4500cb8fc
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2024-05-06 10:43:21 -0300

    Update Secretarias.java

--- herancaClasseObjeto/src/heranca/Secretarias.java
+++ herancaClasseObjeto/src/heranca/Secretarias.java
@@ -4,6 +4,21 @@ public class Secretarias extends Funcionario {
 	
 	private int ramalNumber;
 	
+	
+	public Secretarias() {
+		
+	}
+	
+	public Secretarias(int ramalNumber) {
+		super();
+		this.ramalNumber = ramalNumber;
+		
+	}
+	
+	public void mostrarDados() {
+		super.mostrarDados();
+		System.out.println("Número do Ramal: " + this.ramalNumber);
+	}
 
 	public int getRamalNumber() {
 		return ramalNumber;
