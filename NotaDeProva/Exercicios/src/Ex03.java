c2dc434d8a65afa59d86b695e3e694f61c4009c7
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2024-05-05 22:57:15 -0300

    Update Ex03.java

--- NotaDeProva/Exercicios/src/Ex03.java
+++ NotaDeProva/Exercicios/src/Ex03.java
@@ -1,11 +1,30 @@
 import java.util.Scanner;
 
 public class Ex03 {
-    public static void main(String[] args) throws Exception {
-        Scanner entrada = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+ 		int sorteado = (int) ((10 * Math.random()) + 1);
+		int valor = 0;
+		
+		System.out.println("O numero sorteado é: " + sorteado);
+		System.out.println("");
+		
+		while (valor != sorteado) {
+			
+            System.out.println("Escolha um numero");
+            valor += entrada.nextInt();
+            
+            if (valor == sorteado) {
+                System.out.println("Você acertou!");
+            
+            } else {
+                System.out.println("Você errou!");
+                valor = 0; 
+            }
+        }
+        
+		entrada.close();
+	}
 
-      
-
-        entrada.close();
-    }
 }
