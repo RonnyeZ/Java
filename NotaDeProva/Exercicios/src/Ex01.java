5aedfb776d39384567057cf8450a502b725681da
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2024-05-05 22:58:34 -0300

    Update Ex01.java

--- NotaDeProva/Exercicios/src/Ex01.java
+++ NotaDeProva/Exercicios/src/Ex01.java
@@ -6,13 +6,13 @@ public class Ex01 {
 
         System.out.println("Defina um valor inicial");
         int x = entrada.nextInt();
+        
         System.out.println("Defina um valor final");
         int y = entrada.nextInt();
 
         System.out.println("");
 
         if (y - x <= 1){
-
             System.out.println(0);
 
         } else {
