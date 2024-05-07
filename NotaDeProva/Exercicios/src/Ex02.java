23f8d004600566714b6218d2f5147287abc247a9
RonnyeZ <121881238+RonnyeZ@users.noreply.github.com>
2024-05-07 11:06:50 -0300

    Update Ex02.java

--- NotaDeProva/Exercicios/src/Ex02.java
+++ NotaDeProva/Exercicios/src/Ex02.java
@@ -1,26 +1,31 @@
 import java.util.Scanner;
 
-public class Ex02 {
+public class App {
     public static void main(String[] args) throws Exception {
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("Informe 10 valores");
         
-        int contador = 0;
-        int resultado = 0;
+        int contador = 1;
+        int atual = entrada.nextInt();
+        int resultado = atual;
 
         while (contador < 10){
-            int valor = entrada.nextInt();
-
-            if (valor % 2 == 0){
-                resultado += valor;
+            
+            int proximo = entrada.nextInt();
+            
+            if (atual % 2 == 0){
+                resultado += proximo;
 
             } else {
-                resultado -= valor;
+                resultado -= proximo;
 
             }
 
+            atual = proximo;
+
             contador++;
+            //5 2 8 1 5 6 4 4 6 5
         }
 
         System.out.println(resultado);
